50a67787b3774e5262d40810837857516d6b3385
Maverick Tesini <mavericktesini@gmail.com>
2018-03-19 18:18:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,6 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.create
-    @@all << self.new
-    @@all[@@all.length-1]
-  end
 
   def self.all
     @@all
@@ -13,6 +9,15 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create  #.create
+    @@all << self.new
+    @@all[@@all.length-1]
+  end
+
+  def self.create_by_name #.create_by_name
+    
   end
 
 end
