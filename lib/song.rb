6ac2990126dfeae3b53d23b0d5b3213a64ddacd2
Maverick Tesini <mavericktesini@gmail.com>
2018-03-19 18:11:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,12 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initializes(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+    @@all << name
+  end
+  
   def self.all
     @@all
   end
